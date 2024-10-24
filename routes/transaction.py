897e689593b1451310fc2a5021a638ea4e824dbe
Francisco Santos <franciscosantos@moobitech.com.br>
2024-10-24 20:24:42 -0300

    feat: adicionando listagem, atualização e deleção

--- routes/transaction.py
+++ routes/transaction.py
@@ -5,7 +5,7 @@ from model.transaction import Transaction
 
 
 def register_routes(app):
-	@app.route('/transaction', methods=['POST'])
+	@app.route('/cadastrar/transacao', methods=['POST'])
 	def create_transaction():
 		data = request.get_json()
 
@@ -20,3 +20,49 @@ def register_routes(app):
 		db.session.commit()
 
 		return jsonify({'message': 'Transaction created successfully'}), 201
+
+	@app.route('/listar/transacao', methods=['GET'])
+	def list_transaction():
+		transactions = Transaction.query.all()
+
+		resultado = [{
+			'id': transaction.id,
+			'conta': transaction.conta,
+			'agencia': transaction.agencia,
+			'texto': transaction.texto,
+			'valor': transaction.valor
+	        } for transaction in transactions
+		]
+
+		return jsonify(resultado), 200
+
+	@app.route("/transacao/<int:id>", methods=['GET'])
+	def get_transaction(id):
+		transaction = Transaction.query.get_or_404(id)
+		resultado = transaction.__dict__
+		del resultado['_sa_instance_state']
+
+		return jsonify({'details': resultado}), 200
+
+	@app.route("/delete/transacao/<int:id>", methods=['DELETE'])
+	def delete_transaction(id):
+		transaction = Transaction.query.get_or_404(id)
+		db.session.delete(transaction)
+		db.session.commit()
+
+		return jsonify({'message': "Transação deletada com sucesso"}), 200
+
+	@app.route("/atualizar/transacao/<int:id>", methods=['PUT'])
+	def update_transaction(id):
+		data = request.get_json()
+		transaction = Transaction.query.get_or_404(id)
+
+		transaction.conta = data.get('conta', transaction.conta)
+		transaction.agencia = data.get('agencia', transaction.agencia)
+		transaction.texto = data.get('texto', transaction.texto)
+		transaction.valor = data.get('valor', transaction.valor)
+
+		db.session.add(transaction)
+		db.session.commit()
+
+		return jsonify({'message': "Transacao atualizada com sucesso"}), 200
